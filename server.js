27edad3dc385d4cc88532459508af0fb588eed67
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-05 17:54:31 -0500

    server.js updated: creates api route that reads db.json file

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
+app.get('/api/notes', (req, res) => {
+    res.json(database);
+});
+
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 });
