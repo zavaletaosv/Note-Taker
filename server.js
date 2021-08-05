42be6a8c98d3b4ef7d42531851bc5618d57ecb8e
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-05 17:24:07 -0500

    server.js updated: route to notes.html created

--- server.js
+++ server.js
@@ -11,3 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
