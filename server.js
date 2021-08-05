207cf19ace8e5487a129d6661a64310d6be55a9d
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-05 17:31:18 -0500

    server.js updated: app listening for connections

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ app.use(express.static('public'));
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+});
