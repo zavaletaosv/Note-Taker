0aaac470d8b0193e11d5cb12e4932e8ad59b0471
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-05 17:14:54 -0500

    server.js updated: middleware added

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const database = require('./db/db.json')
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
